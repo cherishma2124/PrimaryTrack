acbb13ad4b448787c216f00f79a4889f77ddef2b
cherishma2124 <cherishmacherry27@gmail.com>
2026-02-20 21:57:50 +0530

    Add files via upload

---
+++ Day6/ShoppingCart/decorators/access.py
@@ -0,0 +1,16 @@
+def login_required(func):
+    def wrapper(*args,**kwargs):
+        is_logged_in=True
+        if is_logged_in:
+            return func(*args,**kwargs)
+        else:
+            print("Please login to add products")
+    return wrapper
+def registration_required(func):
+    def wrapper(*args, **kwargs):
+        is_registered=True
+        if is_registered:
+            return func(*args)
+        else:
+            print("Please register to view cart")
+    return wrapper
